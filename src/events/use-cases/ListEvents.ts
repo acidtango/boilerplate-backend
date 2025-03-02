d7917e4f59634f230d1ec18e1158dc35362c15be
Daniel Ramos <danielramosacosta1@gmail.com>
2025-03-02 20:11:14 +0100

    refactor: remove logger dependency from ListEvents use case

--- src/events/use-cases/ListEvents.ts
+++ src/events/use-cases/ListEvents.ts
@@ -3,28 +3,20 @@ import { UseCase } from '../../shared/domain/models/hex/UseCase.ts'
 import { TalkEvent } from '../domain/models/TalkEvent.ts'
 import type { EventRepository } from '../domain/repositories/EventRepository.ts'
 import { Token } from '../../shared/domain/services/Token.ts'
-import type { Logger } from '../../shared/domain/services/Logger.ts'
 
 export class ListEvents extends UseCase {
   public static async create({ container }: interfaces.Context) {
-    return new ListEvents(
-      await container.getAsync(Token.EVENT_REPOSITORY),
-      container.get(Token.LOGGER)
-    )
+    return new ListEvents(await container.getAsync(Token.EVENT_REPOSITORY))
   }
 
   private readonly eventRepository: EventRepository
 
-  private readonly logger: Logger
-
-  constructor(eventRepository: EventRepository, logger: Logger) {
+  constructor(eventRepository: EventRepository) {
     super()
     this.eventRepository = eventRepository
-    this.logger = logger
   }
 
   async execute(): Promise<TalkEvent[]> {
-    this.logger.info('eseeeeeeeeee')
     return this.eventRepository.findAll()
   }
 }
